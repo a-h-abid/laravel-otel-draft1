8de16eb61db1abd55ec7c7b1a8964ec9eae358b7
Ahmedul Haque Abid <ahmedul.abid@grameenphone.com>
2023-06-17 01:01:38 +0600

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('users/add', function () {
+    $faker = Faker\Factory::create();
+
+    $user = App\Models\User::create([
+        'name' => $faker->name(),
+        'email' => $faker->unique()->safeEmail(),
+        'password' => Hash::make('secret'),
+    ]);
+
+    return [
+        'status' => 'user added',
+        'data' => $user,
+    ];
+});
+
+Route::get('users/list', function () {
+    return Cache::remember('users:all', now()->addSeconds(10), function () {
+        return App\Models\User::all()->toArray();
+    });
 });
